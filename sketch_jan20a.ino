5d8fbedf1f7a1696c4a6ac8d0b27d6fe84d77dbe
SebA-R <123127567+SebA-R@users.noreply.github.com>
2024-01-20 12:12:44 -0500

    updated

--- sketch_jan20a.ino
+++ sketch_jan20a.ino
@@ -19,25 +19,14 @@ void setup() {
   analogWrite(right_wheel_power, 0);
 }
 
-int MIN_SPEED = 0;
-int MAX_SPEED = 100;
-
-int MIN_RAW_SPEED = 160;
-int MAX_RAW_SPEED = 255;
-
 void loop() {
-  // I just set the speeds to 50 to move forward at a constant speed.
-  analogWrite(left_wheel_power, get_raw_speed(50));
-  analogWrite(right_wheel_power, get_raw_speed(50));
-
-  // set direction
+  // Move forward
   go_forwards(left_wheel_1, left_wheel_2);
   go_forwards(right_wheel_1, right_wheel_2);
-}
 
-int get_raw_speed(int speed) {
-    if (speed == 0) return 0;
-    return map(abs(speed), MIN_SPEED, MAX_SPEED, MIN_RAW_SPEED, MAX_RAW_SPEED);
+  // Set a constant speed (you can adjust this value)
+  analogWrite(left_wheel_power, 150);
+  analogWrite(right_wheel_power, 150);
 }
 
 void go_forwards(int wheel_input_1, int wheel_input_2) {
