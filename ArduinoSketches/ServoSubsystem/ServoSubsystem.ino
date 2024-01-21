c9a24adbccc5dbac8bd54eb6048ca380889fff50
pekachoo <jliu6162@gmail.com>
2024-01-21 08:36:50 -0500

    Update ServoSubsystem.ino

--- ArduinoSketches/ServoSubsystem/ServoSubsystem.ino
+++ ArduinoSketches/ServoSubsystem/ServoSubsystem.ino
@@ -5,7 +5,7 @@ Servo servoMotor;
 
 void setup() {
   // Attach the servo to pin 3
-  servoMotor.attach(3);
+  servoMotor.attach(3, 550, 865);
 }
 
 void loop() {
@@ -13,11 +13,11 @@ void loop() {
   servoMotor.write(0);
   delay(1000); // Wait for 1 second
 
-  // // Move the servo to the 90-degree position
-  // servoMotor.write(90);
-  // delay(1000); // Wait for 1 second
+  // Move the servo to the 90-degree position
+  servoMotor.write(90);
+  delay(1000); // Wait for 1 second
 
-  // // Move the servo to the 180-degree position
-  // servoMotor.write(180);
-  // delay(1000); // Wait for 1 second
+  // Move the servo to the 180-degree position
+  servoMotor.write(180);
+  delay(1000); // Wait for 1 second
 }
