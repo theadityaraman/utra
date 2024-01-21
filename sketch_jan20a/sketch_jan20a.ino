cb31e9b9d7fb7ba92b1390901ff5f60cc21e1193
SebA-R <123127567+SebA-R@users.noreply.github.com>
2024-01-20 19:23:04 -0500

    using newping

--- sketch_jan20a/sketch_jan20a.ino
+++ sketch_jan20a/sketch_jan20a.ino
@@ -1,82 +1,99 @@
-// Motor control pins
-int motor1Pin1 = 2;  // Motor 1 control pin 1
-int motor1Pin2 = 3;  // Motor 1 control pin 2
-int motor2Pin1 = 4;  // Motor 2 control pin 1
-int motor2Pin2 = 5;  // Motor 2 control pin 2
+#include <NewPing.h>
+
+// Motor A connections
+const int enA = 9;
+const int in1 = 5;
+const int in2 = 6;
+
+// Motor B connections
+const int enB = 10;
+const int in3 = 7;
+const int in4 = 8;
+
+// Set the speed (0 = off and 255 = max speed)
+const int motorSpeed = 80;
+
+// Define the ultrasonic sensor parameters
+#define TRIGGER_PIN 11
+#define ECHO_PIN 12
+#define MAX_DISTANCE 200 // Maximum distance in centimeters
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
-  // Set the motor control pins as OUTPUT
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
+  Serial.begin(9600);
+
+  // Motor control pins are outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  // Turn off motors - Initial state
+  stop_all();
+
+  // Set the motor speed
+  analogWrite(enA, motorSpeed);
+  analogWrite(enB, motorSpeed);
+
+  delay(200);
+  go_forward();
 }
 
 void loop() {
-  // Move forward
-  moveForward();
-  delay(2000);
+  int distance = sonar.ping_cm();
 
-  // Stop
-  stopMotors();
-  delay(1000);
+  if (distance >= 0 && distance <= 20) { // If obstacle <= 40 centimeters away
+    Serial.println("Obstacle detected ahead");
+    Serial.println(distance);
+    go_backwards();
+    delay(2000);
 
-  // Move backward
-  moveBackward();
-  delay(2000);
-
-  // Stop
-  stopMotors();
-  delay(1000);
-
-  // Turn left
-  turnLeft();
-  delay(1000);
-
-  // Stop
-  stopMotors();
-  delay(1000);
-
-  // Turn right
-  turnRight();
-  delay(1000);
-
-  // Stop
-  stopMotors();
-  delay(1000);
+    // Go left or right to avoid the obstacle
+    if (random(2) == 0) {
+      go_right();
+    } else {
+      go_left();
+    }
+    delay(3000);
+    go_forward();
+  }
+  delay(50); // Wait 50 milliseconds before pinging again
 }
 
-void moveForward() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
+void go_forward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
 
-void moveBackward() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
+void go_backwards() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
-void turnLeft() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
+void go_right() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
-void turnRight() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
+void go_left() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
 
-void stopMotors() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
+void stop_all() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
